d759fcff53fcdbea47dc1f6f098ceb43b2735bee
Aayush <vermaaayush1309@gmail.com>
2022-03-25 02:59:44 +0530

    about-section added | js file added for onscroll event on navigation bar | customaized scroll bar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Services section - Modal
+
+// Portfolio section - Modal
+
+// Our Clients - Swiper
+
+// Website dark/light theme
+
+// Scroll to top button
